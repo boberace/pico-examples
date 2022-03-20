25f7f53f5748e15ae38ed1e7ae966050135f7ae8
boberace <boberace@hotmail.com>
2022-03-20 08:27:48 -0400

    added BNO055 updated bus_scan to look on different pins

---
+++ BNO055/bno055_pico.h
@@ -0,0 +1,57 @@
+/**
+* Copyright (c) 2020 Bosch Sensortec GmbH. All rights reserved.
+*
+* BSD-3-Clause
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the
+*    documentation and/or other materials provided with the distribution.
+*
+* 3. Neither the name of the copyright holder nor the names of its
+*    contributors may be used to endorse or promote products derived from
+*    this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+* STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+* IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+#ifndef bno055_pico_h
+#define bno055_pico_h
+
+#include "bno055.h"
+// #include <stdio.h>
+// #include <stdint.h>
+// #include <string.h>
+// #include "pico/stdlib.h"
+#include "hardware/i2c.h"
+
+class bno055_pico{
+    public:
+        bno055_pico(i2c_inst_t *i2c, uint gpio_sda, uint gpio_scl);
+
+    private:
+    i2c_inst_t *_i2c;
+    uint _gpio_sda, _gpio_scl;
+
+    // void BNO055_delay_msek(u32 msek);
+    // s8 BNO055_I2C_bus_read(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt);
+    // s8 BNO055_I2C_bus_write(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt);
+};
+
+#endif
---
+++ BNO055/main.cpp
@@ -0,0 +1,41 @@
+#include "bno055_pico.h"
+// #include <stdio.h>
+// #include <stdint.h>
+// #include <string.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+// #include "hardware/adc.h"
+// #include <math.h>
+// #include "hardware/pwm.h"
+// #include "hardware/pio.h"
+// #include "hardware/clocks.h"
+// #include "hardware/irq.h"
+
+#define I2C0_BUADRATE 400*1000
+#define I2C_BNO085 i2c0
+
+#define PIN_I2C0_SDA 16
+#define PIN_I2C0_SCL 17
+
+
+void setup_i2c0(void);
+
+
+int main() {
+    stdio_init_all();
+    setup_i2c0();
+
+    // while(true){
+
+
+    // }
+    return 0;
+}
+
+void setup_i2c0(void){
+    i2c_init(i2c0, I2C0_BUADRATE);
+    gpio_set_function(PIN_I2C0_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(PIN_I2C0_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(PIN_I2C0_SDA);
+    gpio_pull_up(PIN_I2C0_SCL);
+}
