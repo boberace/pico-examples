d805b73bbb708778b2e5e359bd3f524700461e4d
rrace001 <rrace001@hotmail.com>
2022-04-03 15:36:33 -0400

    updated rotation vector

--- i2c/bno085_i2c/main.c
+++ i2c/bno085_i2c/main.c
@@ -1,6 +1,6 @@
 /*
 
-Docs:
+Documents referenced in comments:
 DS: BNO080/85/86 Data Sheet
 RM: SH-2 Reference Manual v1.2
 TP: Sensor Hub Transport Protocol R 1.7
@@ -15,6 +15,7 @@ TP: Sensor Hub Transport Protocol R 1.7
 #include "sh2_SensorValue.h"
 #include "sh2_err.h"
 
+// uncomment to print debug information over usb serial
 // #define DEBUG
 
 #ifdef DEBUG
@@ -23,36 +24,30 @@ TP: Sensor Hub Transport Protocol R 1.7
 # define DEBUG_PRINT(...)
 #endif
 
+// define interrupt and reset pins
 #define PIN_BNO085_INT 15
 #define PIN_BNO085_RST 16
 
+// define i2c pararmeters and pins
 #define I2C0_BUADRATE 100*1000
 #define PIN_I2C0_SDA 4
 #define PIN_I2C0_SCL 5
 i2c_inst_t *I2C_BNO085 = i2c0;
-#define I2C_BUFFER_LIMIT 32
-
 #define BNO085_I2C_ADDR 0x4A
 
+// sh2 variables
 sh2_Hal_t sh2_hal;
-
-#define CARGO_SIZE  SH2_HAL_MAX_PAYLOAD_IN // I2C_BUFFER_LIMIT
-uint8_t shtp_header[4];
-uint8_t shtp_cargo[CARGO_SIZE]; 	
-bool reset_occurred = false;
+#define CARGO_SIZE  128 // arbitrarily set, can be up to (2^15 - 4), 
+bool reset_occurred = false; // flag set in hal_callback when reset occurs
 sh2_ProductIds_t prodIds; ///< The product IDs returned by the sensor
+sh2_SensorValue_t sensorValue;
+const sh2_SensorId_t sensorID = SH2_ROTATION_VECTOR;
+const float reportFrequency = 400;
 
+// global return variable 
 int16_t ret = 0;
 
-//DS: 1.3.1 SHTP - The BNO08X supports 6 channels
-const uint8_t CHANNEL_COMMAND = 0;
-const uint8_t CHANNEL_EXECUTABLE = 1;
-const uint8_t CHANNEL_CONTROL = 2;
-const uint8_t CHANNEL_REPORTS = 3;
-const uint8_t CHANNEL_WAKE_REPORTS = 4;
-const uint8_t CHANNEL_GYRO = 5;
-
-
+// function headers
 void setup_i2c0(void);
 int i2c_open(sh2_Hal_t *self);
 void i2c_close(sh2_Hal_t *self);
@@ -61,31 +56,55 @@ int i2c_write(sh2_Hal_t *self, uint8_t *pBuffer, unsigned len);
 uint32_t getTimeUs(sh2_Hal_t *self);
 static void hal_callback(void *cookie, sh2_AsyncEvent_t *pEvent);
 bool bno085_init_i2c(void);
+bool enableReport(sh2_SensorId_t sensorId, float frequency);
+bool getSensorEvent();
+static void sensorHandler(void *cookie, sh2_SensorEvent_t *event);
 
-int main() {    
-    stdio_init_all();
-    DEBUG_PRINT("\nStarted\n");
-    setup_i2c0();
-    bool r = bno085_init_i2c();
-    if(r == false){
-        printf(" bno0855 will not initialize\n");
-        return false;
-    } else {
-        printf(" bno0855 has initialized\n");
-    }
-
-  for (int n = 0; n < prodIds.numEntries; n++) {
-    printf("Part %d\n", prodIds.entry[n].swPartNumber);
-    printf(": Version %d.%d.%d\n", prodIds.entry[n].swVersionMajor, prodIds.entry[n].swVersionMinor, prodIds.entry[n].swVersionPatch);
-    printf(" Build %d\n", prodIds.entry[n].swBuildNumber);
+void setReports(void) {
+  printf("Setting desired reports\n");
+  if (! enableReport(sensorID, reportFrequency)) {
+    printf("Could not enable game vector\n");
   }
-    
+}
 
-     while(false){     
+int main() {    //  main  main  main  main  main  main  main  main  main  main  main  main  main  main  main  main
+    stdio_init_all(); DEBUG_PRINT("\nStarted\n");
+    setup_i2c0();
+    if(bno085_init_i2c()) printf(" bno0855 has initialized\n");
+    else printf(" bno0855 NOT initialize\n");
 
-        sleep_ms(300);            
+    for (int n = 0; n < prodIds.numEntries; n++) {
+    printf("Part %d\n", prodIds.entry[n].swPartNumber);
+    printf(": Version %d.%d.%d\n",  prodIds.entry[n].swVersionMajor, 
+                                    prodIds.entry[n].swVersionMinor, 
+                                    prodIds.entry[n].swVersionPatch);
+    printf(" Build %d\n", prodIds.entry[n].swBuildNumber);
+    }  
+
+    setReports(); DEBUG_PRINT("setReports\n");
+
+
+    while(true){     //   loop   loop   loop   loop   loop   loop   loop   loop   loop   loop   loop   loop   loop
+
+        sleep_ms(1);
+
+        if (getSensorEvent()) {
+
+            switch (sensorValue.sensorId) {
+            
+            case sensorID:
+            printf("Rotation Vector:  i:%f, j:%f, k:%f,  r:%f\n",  sensorValue.un.gameRotationVector.i,
+                                                                    sensorValue.un.gameRotationVector.j,
+                                                                    sensorValue.un.gameRotationVector.k,
+                                                                    sensorValue.un.gameRotationVector.real);
+            break;
+            }
+        }
+
+        // if (reset_occurred) enableReport(sensorID, reportFrequency);
 
     }
+    
     return 0;
 }
 
@@ -107,25 +126,21 @@ static void hal_callback(void *cookie, sh2_AsyncEvent_t *pEvent) {
 
 bool bno085_init_i2c(void){
 
-    // interupt pin
+    // interupt and RST pins
     gpio_init(PIN_BNO085_INT);
     gpio_set_function(PIN_BNO085_INT, GPIO_OUT);
     gpio_init(PIN_BNO085_RST);
     gpio_set_function(PIN_BNO085_RST, GPIO_OUT);
 
+    // read a single byte to determine if connected - 
+    // this could cause a full report to generate that would have to be flushed
+    // but no matter as this is followed with a hardware reset
     uint8_t dummy;
     ret = i2c_read_blocking(I2C_BNO085, BNO085_I2C_ADDR, &dummy, 1, false);
     if(ret < 1){
          printf(" BNO085 will not connect\n");
          return false;
-    }
-        DEBUG_PRINT("i2c_read_blocking dummy test if connected %d\n", ret);
-
-    sh2_hal.open = i2c_open;
-    sh2_hal.close = i2c_close;
-    sh2_hal.read = i2c_read;
-    sh2_hal.write = i2c_write;
-    sh2_hal.getTimeUs = getTimeUs; 
+    } DEBUG_PRINT("i2c_read_blocking dummy test if connected %d\n", ret);
 
     // hardware reset
     gpio_put(PIN_BNO085_RST, 1);
@@ -135,26 +150,38 @@ bool bno085_init_i2c(void){
     gpio_put(PIN_BNO085_RST, 1);
     sleep_ms(10);  
 
+    // setup sh2 hal pointers
+    sh2_hal.open = i2c_open;
+    sh2_hal.close = i2c_close;
+    sh2_hal.read = i2c_read;
+    sh2_hal.write = i2c_write;
+    sh2_hal.getTimeUs = getTimeUs;  
+
+    //
     int status;
     status = sh2_open(&sh2_hal, hal_callback, NULL);
     if (status != SH2_OK) { 
         return false;
-    }        
-    DEBUG_PRINT("---------------------------------------------------- sh2_open \n");
+    } DEBUG_PRINT("---------------------------------------------------- sh2_open \n");
+
     memset(&prodIds, 0, sizeof(prodIds));
     status = sh2_getProdIds(&prodIds);
     if (status != SH2_OK) {
         return false;
-    }
-    DEBUG_PRINT("------------------------------------------------ sh2_getProdIds \n");
+    } DEBUG_PRINT("------------------------------------------------ sh2_getProdIds \n");
+
+    // Register sensor listener
+    sh2_setSensorCallback(sensorHandler, NULL);
 
     return true;
 }
 
-int i2c_open(sh2_Hal_t *self){  
+int i2c_open(sh2_Hal_t *self){
     DEBUG_PRINT("************************************ i2c_open \n");
+    uint8_t shtp_header[4]; // DS: 1.3.1 SHTP
+    uint8_t shtp_cargo[CARGO_SIZE]; 	
     // send a software reset = { Length LSB, Length LSB, Channel, SeqNum, data }
-    uint8_t softreset_pkt[5] = { 5, 0, CHANNEL_EXECUTABLE, 0, 1}; // DS: Figure 1-27: 1 – reset
+    uint8_t softreset_pkt[5] = { 5, 0, 1, 0, 1}; // DS: Figure 1-27: 1 – reset
     ret = i2c_write_blocking(I2C_BNO085,BNO085_I2C_ADDR, softreset_pkt, 5, false );
         DEBUG_PRINT("i2c_write_blocking softreset_pkt %d\n", ret); 
     if(ret < 1) return -1;
@@ -182,6 +209,7 @@ void i2c_close(sh2_Hal_t *self){
 
 int i2c_read(sh2_Hal_t *self, uint8_t *pBuffer, unsigned len, uint32_t *t_us){
     DEBUG_PRINT("************************************ i2c_read \n");
+    uint8_t shtp_header[4]; // DS: 1.3.1 SHTP	
     ret = i2c_read_blocking(I2C_BNO085,BNO085_I2C_ADDR, shtp_header, 4, false );
         DEBUG_PRINT("i2c_read_blocking shtp_header %d\n", ret);
     if(ret != 4) return 0;
@@ -249,4 +277,52 @@ int i2c_write(sh2_Hal_t *self, uint8_t *pBuffer, unsigned len){
 uint32_t getTimeUs(sh2_Hal_t *self){
     DEBUG_PRINT("************************************ getTimeUs \n");
     return to_us_since_boot(get_absolute_time());
+}
+
+bool enableReport(sh2_SensorId_t sensorId, float frequency) {
+  uint32_t interval_us = (uint32_t)(1000000 / frequency);
+  static sh2_SensorConfig_t config;
+
+  // These sensor options are disabled or not used in most cases
+  config.changeSensitivityEnabled = false;
+  config.wakeupEnabled = false;
+  config.changeSensitivityRelative = false;
+  config.alwaysOnEnabled = false;
+  config.changeSensitivity = 0;
+  config.batchInterval_us = 0;
+  config.sensorSpecific = 0;
+
+  config.reportInterval_us = interval_us;
+  int status = sh2_setSensorConfig(sensorId, &config);
+
+  if (status != SH2_OK) {
+    return false;
+  }
+
+  return true;
+}
+
+bool getSensorEvent() {
+
+  sensorValue.timestamp = 0;
+
+  sh2_service();
+
+  if (sensorValue.timestamp == 0 && sensorValue.sensorId != SH2_GYRO_INTEGRATED_RV) {
+    // no new events
+    return false;
+  }
+
+  return true;
+}
+
+// Handle sensor events.
+static void sensorHandler(void *cookie, sh2_SensorEvent_t *event) {
+
+  ret = sh2_decodeSensorEvent(&sensorValue, event);
+  if (ret != SH2_OK) {
+    printf("BNO08x - Error decoding sensor event");
+    sensorValue.timestamp = 0;
+    return;
+  }
 }
