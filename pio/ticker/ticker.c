4247d9e3a7337f1ef56a6792dbf135945a137811
boberace <boberace@hotmail.com>
2022-08-18 18:57:43 -0400

    setting up pio

--- pio/ticker/ticker.c
+++ pio/ticker/ticker.c
@@ -1,29 +1,77 @@
+/*
+
+Program to test speed of pio 
+
+todo:  create alternate to connect two statementions to respond to each other
+
+*/
+
 #include <stdio.h>
 
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
-#include "ticker.pio.h"
+#include "hardware/irq.h"
+#include "blink.pio.h"
+#include "hall.pio.h"
+// three pins in a row are used starting with PIN_LED 
+// second pin blinks with LED which is connected to 
+// thrid pin that is used to detect the LED blink
+const int PIN_LED = 25;
+const int PIN_HALL = 27;
 
-void ticker_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq);
+const int SM_HALL = 0;
+PIO PIO_HALL = pio0;
+
+const int SM_BLINK = 0;
+PIO PIO_BLINK = pio1;
+
+void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq);
+void hall_detect(PIO pio, uint sm, uint offset, uint pin_hall);
+void hall_isr0();
+void hall_isr1();
 
 int main() {
-    setup_default_uart();
+    stdio_init_all(); 
 
-    // todo get free sm
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &ticker_program);
-    printf("Loaded program at %d\n", offset);
+    uint offset_blink = pio_add_program(PIO_BLINK, &blink_program);  
+    blink_pin_forever(PIO_BLINK, SM_BLINK, offset_blink, PIN_LED, 1); 
 
-    ticker_pin_forever(pio, 0, offset, 0, 3);
-    ticker_pin_forever(pio, 1, offset, 6, 4);
-    ticker_pin_forever(pio, 2, offset, 11, 1);
+    uint offset_hall = pio_add_program(PIO_HALL, &hall_program);    
+    hall_detect(PIO_HALL, SM_HALL, offset_hall, PIN_HALL);
+
+    while(1){
+        printf("tick\n");
+        sleep_ms(1000);
+        printf("tock\n");
+        sleep_ms(1000);
+    }
 }
 
-void ticker_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-    ticker_program_init(pio, sm, offset, pin);
+void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
+    blink_program_init(pio, sm, offset, pin);
     pio_sm_set_enabled(pio, sm, true);
 
-    printf("tickering pin %d at %d Hz\n", pin, freq);
-    pio->txf[sm] = clock_get_hz(clk_sys) / (2 * freq);
+    printf("Blinking pin %d at %d Hz\n", pin, freq);
+
+    // PIO counter program takes 3 more cycles in total than we pass as
+    // input (wait for n + 1; mov; jmp)
+    pio->txf[sm] = (clock_get_hz(clk_sys) / (2 * freq)) - 3;
+}
+
+void hall_detect(PIO pio, uint sm, uint offset, uint pin_hall) {
+    hall_program_init(pio, sm, offset, pin_hall);
+    pio_sm_set_enabled(pio, sm, true);
+}
+
+void hall_isr0()
+{   
+    printf("low");
+    pio_interrupt_clear(PIO_HALL, 0);
+}
+
+void hall_isr1()
+{   
+    printf("high");
+    pio_interrupt_clear(PIO_HALL, 1);
 }
