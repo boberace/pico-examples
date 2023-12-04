cb830c012e5de8a90177ed8799f3aa5d66485525
boberace <boberace@github.com>
2023-12-04 13:52:28 -0500

    tof8_imu - have the eight vl53l5xc sensors reporting. Very slow - About 7 hz aquisition but the uros usb is slow to about 3 hz Need to get this to the report 15 hz capability of the vl53l5cx

--- uros/tof8_imu_uros_usb/tof8_imu_uros_usb.cpp
+++ uros/tof8_imu_uros_usb/tof8_imu_uros_usb.cpp
@@ -31,12 +31,12 @@ extern "C" {
 #define led_toggle() gpio_put(25, !gpio_get(25))
 #endif
 
+
 // define i2c pararmeters and pins
 #define I2CA_BUADRATE 1000*1000
 #define PIN_I2CA_SDA 12
 #define PIN_I2CA_SCL 13
 #define I2CA_INSTANCE i2c0
-static const uint8_t LP_GPIO[]={22,27,28,10,11,15,20,21};
 
 char id_data[] = "pico_vl53l5cx";
 
@@ -46,10 +46,10 @@ char id_data[] = "pico_vl53l5cx";
 #define UART_A_TX_PIN 8
 #define UART_A_RX_PIN 9
 
-#define TOF_FREQ 10
+#define TOF_FREQ 10 // Hz
 #define TOF_MS 1000/TOF_FREQ
 
-#define BLINK_ERROR_FREQ 10
+#define BLINK_ERROR_FREQ 15
 #define BLINK_NORMAL_FREQ 1
 
 static mutex_t print_mutex;
@@ -69,19 +69,37 @@ static mutex_t print_mutex;
 rcl_publisher_t publisher;
 sensor_msgs__msg__PointCloud2 pc2_msg;  
 static mutex_t tof_msg_mutex;
+
+// number of tof sensors
+#define NUM_SENSORS 8
+static const uint8_t LP_GPIO[NUM_SENSORS]={22,27,28,10,11,15,20,21}; // Comms enable pin
+
+// tof sensor
 #define WIDTH 8
 #define HEIGHT 8
-#define POINT_STEP (sizeof(float) * 3) // Size of each point (x, y, z)
-#define ROW_STEP (POINT_STEP * WIDTH) // Size of each row
-#define DATA_SIZE (HEIGHT * ROW_STEP) // Total size of the data array
-uint8_t pointcloud_data[DATA_SIZE]; // Static array for point cloud data
 #define NUM_FIELDS 3 // x, y, z
+
+// pointcloud2 data
+#define WIDTH_ARRAY (WIDTH * NUM_SENSORS)
+#define FIELD_SIZE (sizeof(float)) // Size of each field (x, y, z)
+#define POINT_STEP (FIELD_SIZE * NUM_FIELDS) // Size of each point (x, y, z)
+#define ROW_STEP (POINT_STEP * WIDTH_ARRAY) // Size of each row of array
+#define DATA_SIZE (HEIGHT * ROW_STEP) // Total size of the data byte array of pointcloud2 message
+uint8_t pointcloud_data[DATA_SIZE]; // Static array for pointcloud2 data
+#define SENSOR_BYTE_OFFSET (POINT_STEP * WIDTH * HEIGHT) // Byte Offset for sensor data in pointcloud2 message
+
 sensor_msgs__msg__PointField point_fields[NUM_FIELDS];
 
 uint blink_interval = BLINK_NORMAL;
 uint imu_event_counter = 0;
 uint publish_event_counter = 0;
 uint timer_calback_counter = 0;
+
+uint64_t curr_millis_inside = 0;
+uint64_t prev_millis_inside = 0;
+
+uint64_t curr_millis_outside = 0;
+uint64_t prev_millis_outside = 0;
 
 uint setup_uart(){
     uint uart_ret = uart_init(UART_A_ID, UART_A_BAUD_RATE);
@@ -116,18 +134,17 @@ void init_point_cloud2_message(){
     // define data for point cloud2 message
     pc2_msg.header.frame_id.data = id_data;
     
-    pc2_msg.height = WIDTH;
-    pc2_msg.width = HEIGHT;    
+    pc2_msg.height = HEIGHT;
+    pc2_msg.width = WIDTH_ARRAY;    
     pc2_msg.point_step = POINT_STEP;
     pc2_msg.row_step = ROW_STEP;
     pc2_msg.data.size = DATA_SIZE;
     pc2_msg.data.data = pointcloud_data;
     pc2_msg.is_bigendian = false; // RP2040 is little endian (Cortex M0+)
-    pc2_msg.is_dense = false;
+    pc2_msg.is_dense = false; // todo: check if all points are valid
 
     // Define fields for x, y, z
-    pc2_msg.fields.size = NUM_FIELDS; 
-    // pc2_msg.fields.data = malloc(sizeof(sensor_msgs__msg__PointField) * pc2_msg.fields.size);
+    pc2_msg.fields.size = NUM_FIELDS;   
 
     sensor_msgs__msg__PointField__init(&point_fields[0]);
     rosidl_runtime_c__String__assign(&point_fields[0].name, "x");
@@ -149,18 +166,41 @@ void init_point_cloud2_message(){
 
     pc2_msg.fields.data = point_fields;
 }
+// rotatez - takes x,y,z and rotates around z axis by angle
+void rotatez(float *x, float *y, float *z, float angle){
+    float x1 = *x;
+    float y1 = *y;
+    float z1 = *z;
+    *x = x1 * cos(angle) - y1 * sin(angle);
+    *y = x1 * sin(angle) + y1 * cos(angle);
+    *z = z1;
+}
 
+void deselect_sensor_all(){
+    for(uint i=0; i<NUM_SENSORS; i++){
+        gpio_put(LP_GPIO[i], 0);
+    }    
+}
+
+void select_sensor_all(){
+    for(uint i=0; i<NUM_SENSORS; i++){
+        gpio_put(LP_GPIO[i], 1);
+    }    
+}
+
+void select_sensor(uint s){
+    deselect_sensor_all();
+    gpio_put(LP_GPIO[s], 1);
+}
 
 void core1_entry(){ 
     PRINT("\r\n-------------------------------core1_entry-------------------------------\r\n");
 
-    // float cc[]={-0.3368898534,-0.2429801799,-0.1467304745,-0.04906767433,
-    //                     0.04906767433, 0.1467304745, 0.2429801799, 0.3368898534};
-    // float ss[]={  0.3368898534,   0.2429801799,   0.1467304745,   0.04906767433,
-    //                     -0.04906767433, -0.1467304745, -0.2429801799,   -0.3368898534};
-    float cc[8], ss[8];
+    // precalculate the sin and cos values for the grid
+    // todo - update to use WIDTH and HEIGHT (check for float division)
+    float cc[WIDTH], ss[HEIGHT];
     float sfov = M_PI_4; 
-    float pfov = sfov / 8.0f;
+    float pfov = sfov / 8.0f; 
     float offest = sfov*7/16;                     
     for(int i=0 ; i<8; i++){ 
             cc[i] = cos(i*pfov - offest - M_PI_2);
@@ -171,146 +211,192 @@ void core1_entry(){
 	/*   VL53L5CX ranging variables  */
 	/*********************************/
 
-	uint8_t 				status, isAlive, isReady;
-    uint32_t 				integration_time_ms;
-	VL53L5CX_Configuration 	Dev;			/* Sensor configuration */
-	VL53L5CX_ResultsData 	Results;		/* Results data from VL53L5CX */
+	uint8_t 				status[NUM_SENSORS], isAlive[NUM_SENSORS], isReady[NUM_SENSORS];
+    uint32_t 				integration_time_ms[NUM_SENSORS];
+	VL53L5CX_Configuration 	Dev[NUM_SENSORS];			/* Sensor configuration */
+	VL53L5CX_ResultsData 	Results[NUM_SENSORS];		/* Results[i] data from VL53L5CX */
 
     setup_i2cA();
-    // setup LP mode comms enable for all 8 sensors
-    for(uint i=0; i<8; i++){
+    // setup LP mode comms enable for all sensors
+    for(uint i=0; i<NUM_SENSORS; i++){
         gpio_init(LP_GPIO[i]);
         gpio_set_dir(LP_GPIO[i], GPIO_OUT);
         gpio_put(LP_GPIO[i], 0);
-    }    
-    // enable LP mode comms for the first sensor - this is the only one we will use
-	gpio_put(LP_GPIO[0], 1);
+    }        
 
-    Dev.platform.i2c_instance = I2CA_INSTANCE;
-    Dev.platform.address = VL53L5CX_DEFAULT_I2C_ADDRESS;
+    for(uint i=0; i<NUM_SENSORS; i++){
 
-	/*********************************/
-	/*   Power on sensor and init    */
-	/*********************************/
+        select_sensor(i);
 
-	/* (Optional) Check if there is a VL53L5CX sensor connected */
-	status = vl53l5cx_is_alive(&Dev, &isAlive);
-	if(!isAlive || status)
-	{
-		PRINT("VL53L5CX not detected at requested address\r\n");
-		return; // status;
-	}
+        Dev[i].platform.i2c_instance = I2CA_INSTANCE;
+        Dev[i].platform.address = VL53L5CX_DEFAULT_I2C_ADDRESS; 
+        status[i] = vl53l5cx_set_i2c_address(&Dev[i], 0x20 + 0x02*i); // change the default address for each sensor
 
-	/* (Mandatory) Init VL53L5CX sensor */
-	status = vl53l5cx_init(&Dev);
-	if(status)
-	{
-		PRINT("VL53L5CX ULD Loading failed\r\n");
-		return; // status;
-	}
+        /*********************************/
+        /*   Power on sensor and init    */
+        /*********************************/
 
-	PRINT("VL53L5CX ULD ready ! (Version : %s)\r\n",
-			VL53L5CX_API_REVISION);
+        /* (Optional) Check if there is a VL53L5CX sensor connected */
+        status[i] = vl53l5cx_is_alive(&Dev[i], &isAlive[i]);
+        if(!isAlive[i] || status[i])
+        {
+            PRINT("VL53L5CX not detected at requested address - sensor %i\r\n", i);
+            return; 
+        }
+
+        /* (Mandatory) Init VL53L5CX sensor */
+        status[i] = vl53l5cx_init(&Dev[i]);
+        if(status[i])
+        {
+            PRINT("VL53L5CX ULD Loading failed - sensor %i\r\n", i);
+            return; 
+        }
+
+        PRINT("VL53L5CX ULD ready ! (Version : %s) - sensor %i\r\n", VL53L5CX_API_REVISION, i);
 
 
 
-	/*********************************/
-	/*        Set some params        */
-	/*********************************/
+        /*********************************/
+        /*        Set some params        */
+        /*********************************/
 
-	/* Set resolution in 8x8. WARNING : As others settings depend to this
-	 * one, it must be the first to use.
-	 */
-	status = vl53l5cx_set_resolution(&Dev, VL53L5CX_RESOLUTION_8X8);
-	if(status)
-	{
-		PRINT("vl53l5cx_set_resolution failed, status %u\r\n", status);
-		return; // status;
-	}
+        /* Set resolution in 8x8. WARNING : As others settings depend to this
+        * one, it must be the first to use.
+        */
+        // todo - Use WIDTH and HEIGHT to select resolution
+        status[i] = vl53l5cx_set_resolution(&Dev[i], VL53L5CX_RESOLUTION_8X8);
+        if(status[i])
+        {
+            PRINT("vl53l5cx_set_resolution failed, status %u - sensor %i\r\n", status[i], i);
+            return;
+        }
 
-	/* Set ranging frequency to 10Hz.
-	 * Using 4x4, min frequency is 1Hz and max is 60Hz
-	 * Using 8x8, min frequency is 1Hz and max is 15Hz
-	 */
-	status = vl53l5cx_set_ranging_frequency_hz(&Dev, 10);
-	if(status)
-	{
-		PRINT("vl53l5cx_set_ranging_frequency_hz failed, status %u\r\n", status);
-		return; // status;
-	}
+        /* Set ranging frequency to 10Hz.
+        * Using 4x4, min frequency is 1Hz and max is 60Hz
+        * Using 8x8, min frequency is 1Hz and max is 15Hz
+        */
+        status[i] = vl53l5cx_set_ranging_frequency_hz(&Dev[i], TOF_FREQ);
+        if(status[i])
+        {
+            PRINT("vl53l5cx_set_ranging_frequency_hz failed, status[i] %u - sensor %i \r\n", status[i], i);
+            return; // status[i];
+        }
 
-	/* Set target order to closest */
-	status = vl53l5cx_set_target_order(&Dev, VL53L5CX_TARGET_ORDER_CLOSEST);
-	if(status)
-	{
-		PRINT("vl53l5cx_set_target_order failed, status %u\r\n", status);
-		return; // status;
-	}
+        /* Set target order to closest */
+        status[i] = vl53l5cx_set_target_order(&Dev[i], VL53L5CX_TARGET_ORDER_CLOSEST);
+        if(status[i])
+        {
+            PRINT("vl53l5cx_set_target_order failed, status[i] %u - sensor %i \r\n", status[i], 1);
+            return; // status[i];
+        }
 
-	/* Get current integration time */
-	status = vl53l5cx_get_integration_time_ms(&Dev, &integration_time_ms);
-	if(status)
-	{
-		PRINT("vl53l5cx_get_integration_time_ms failed, status %u\r\n", status);
-		return; // status;
-	}
-	PRINT("Current integration time is : %d ms\r\n", integration_time_ms);
+        /*********************************/
+        /*  Set ranging mode autonomous  */
+        /*********************************/
 
+        // status[i] = vl53l5cx_set_ranging_mode(&Dev[i], VL53L5CX_RANGING_MODE_AUTONOMOUS);
+        // if(status[i])
+        // {
+        //     printf("vl53l5cx_set_ranging_mode failed, status %u - sensor %i \n", status[i], i);
+        //     return;
+        // }
+
+        /* Using autonomous mode, the integration time can be updated (not possible
+        * using continuous) */
+        // status[i] = vl53l5cx_set_integration_time_ms(&Dev[i], 20);
+
+        /* Get current integration time */
+        status[i] = vl53l5cx_get_integration_time_ms(&Dev[i], &integration_time_ms[i]);
+        if(status[i])
+        {
+            PRINT("vl53l5cx_get_integration_time_ms failed, status[i] %u - sensor %i \r\n", status[i], i);
+            return;
+        }
+        PRINT("Current integration time is : %d ms\r\n", integration_time_ms[i]);    
+
+        status[i] = vl53l5cx_start_ranging(&Dev[i]);
+        
+        PRINT("vl53l5cx start status[i] %u - sensor %i\r\n", status[i], i);   
+        
+    }
+    deselect_sensor_all();
+    select_sensor_all();
 	/*********************************/
 	/*         Ranging loop          */
-	/*********************************/
-
-	status = vl53l5cx_start_ranging(&Dev);
+	/*********************************/	
 
     while (true)
     {
-		status = vl53l5cx_check_data_ready(&Dev, &isReady);
+        prev_millis_outside = to_ms_since_boot(get_absolute_time());
+        mutex_enter_blocking(&tof_msg_mutex);
+        prev_millis_inside = to_ms_since_boot(get_absolute_time());
+        for(uint i=0; i<NUM_SENSORS; i++){
+            
+            status[i] = vl53l5cx_check_data_ready(&Dev[i], &isReady[i]);
+            // PRINT("vl53l5cx_check_data_ready status[i] %u - sensor %i\r\n", status[i], i);
 
-		if(isReady)
-		{
+            while(isReady[i]==0){
+                status[i] = vl53l5cx_check_data_ready(&Dev[i], &isReady[i]);
+                // PRINT(".");
+                sleep_ms(1);
+            }
 
-			vl53l5cx_get_ranging_data(&Dev, &Results);
+            if(isReady[i])
+            {
 
-            mutex_enter_blocking(&tof_msg_mutex);
+                vl53l5cx_get_ranging_data(&Dev[i], &Results[i]);                
 
-                // pc2_msg size and fields are initialized in the Core0 main function
-                // todo: check for timestamp in sensor message and use it if it exists
+                    // pc2_msg size and fields are initialized in the Core0 main function
+                    // todo: check for timestamp in sensor message and use it if it exists
 
-                rcutils_time_point_value_t now;
-                if(RCUTILS_RET_OK == rcutils_system_time_now(&now)){      
-                    pc2_msg.header.stamp.sec = RCUTILS_NS_TO_S(now);
-                    pc2_msg.header.stamp.nanosec = now - RCUTILS_S_TO_NS(pc2_msg.header.stamp.sec);
-                }
-                int pindex = 0;
-                for(uint w=0; w<8; w++){
-                    for(uint h=0; h<8; h++){                        
-                        pindex = w*8+h;
-                        float d_meter = Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*pindex] / 1000.0f;
-                        d_meter = d_meter > 0 ? d_meter : 0;
-                        // sensor distance is normal to plane and not radial. 
-                        float z = d_meter * cc[w];
-                        float x = d_meter * ss[h];
-                        float y = d_meter;
-
-                        size_t bindex = pindex * POINT_STEP;
-                        memcpy(&pointcloud_data[bindex], &x, sizeof(float));
-                        memcpy(&pointcloud_data[bindex + sizeof(float)], &y, sizeof(float));
-                        memcpy(&pointcloud_data[bindex + 2 * sizeof(float)], &z, sizeof(float));  
+                    rcutils_time_point_value_t now;
+                    if(RCUTILS_RET_OK == rcutils_system_time_now(&now)){      
+                        pc2_msg.header.stamp.sec = RCUTILS_NS_TO_S(now);
+                        pc2_msg.header.stamp.nanosec = now - RCUTILS_S_TO_NS(pc2_msg.header.stamp.sec);
                     }
-                }           
-  
-            mutex_exit(&tof_msg_mutex);     
+                    int pindex = 0;
+                    for(uint w=0; w<8; w++){
+                        for(uint h=0; h<8; h++){                        
+                            pindex = w*8+h;
+                            float d_meter = Results[i].distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*pindex] / 1000.0f;
+                            d_meter = d_meter > 0 ? d_meter : 0;
+                            // sensor distance is normal to plane and not radial. 
+                            float z = d_meter * cc[w];
+                            float x = d_meter * ss[h];
+                            float y = d_meter;
 
-		} 
+                            rotatez(&x, &y, &z, i*M_PI_4); 
 
-		/* Wait a few ms to avoid too high polling (function in platform
-		 * file, not in API) */
-		WaitMs(&(Dev.platform), 3);        
+                            size_t bindex = pindex * POINT_STEP;
+                            memcpy(&pointcloud_data[bindex + i*SENSOR_BYTE_OFFSET], &x, sizeof(float));
+                            memcpy(&pointcloud_data[bindex + sizeof(float) + i*SENSOR_BYTE_OFFSET], &y, sizeof(float));
+                            memcpy(&pointcloud_data[bindex + 2 * sizeof(float) + i*SENSOR_BYTE_OFFSET], &z, sizeof(float));  
+                        }
+                    }                                  
+
+            } else {
+                PRINT("VL53L5CX not ready - sensor %i\r\n", i);
+            }
+
+        } 
+        curr_millis_inside = to_ms_since_boot(get_absolute_time());
+        mutex_exit(&tof_msg_mutex); 
+        curr_millis_outside = to_ms_since_boot(get_absolute_time());
+        uint32_t delta_millis_inside = (curr_millis_inside - prev_millis_inside);
+        uint32_t delta_millis_outside = (curr_millis_outside - prev_millis_outside);
+        PRINT("delta_millis inside %u\r\n", delta_millis_inside);
+        PRINT("delta_millis outside %u\r\n\r\n", delta_millis_outside);
+        sleep_ms(1);
+        // /* Wait a few ms to avoid too high polling (function in platform
+        //     * file, not in API) */
+        // WaitMs(&(Dev[i].platform), 3);        
 
     }
 
-    status = vl53l5cx_stop_ranging(&Dev);
-	PRINT("vl53l5cx stop status %i\r\n", status);
+    for(uint i=0; i<8; i++){
+        status[i] = vl53l5cx_stop_ranging(&Dev[i]);
+        PRINT("vl53l5cx stop status[i] %i - sensor %i\r\n", status[i], i);
+    }
     
 }
 
