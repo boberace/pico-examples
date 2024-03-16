6dd38615e807b062e091ec64d51b00069f29e97e
boberace <boberace@github.com>
2024-03-16 03:31:52 +0000

    added full speed test

--- mtr_ctrl/mtr_ctrl_s1.c
+++ mtr_ctrl/mtr_ctrl_s1.c
@@ -6,23 +6,122 @@
 #include "hardware/timer.h"
 #include "pin_blink.pio.h"
 
-#define PIN_CA1 14
-#define PIN_CA2 15
+#define PIN_CA1 13
+#define PIN_CA2 12
+#define PIN_MTR_ENA 16
 
-#define PIN_HALL_D1 13
-#define PIN_HALL_D2 14
-
-#define PIN_HALL_A1 26
-#define PIN_HALL_A2 27
+#define PIN_HALL_CEN 15
+#define PIN_HALL_BTW 14
 
 #define PIN_PIEZO 28
 
 #define PIN_STROBE 16
+#define PIN_LED 25
 
-
+bool clockwise = true;
+bool hall_cen_initial = false;
+bool hall_btw_initial = false;
+bool hall_cen = false;
+bool hall_btw = false;
+bool state = false;
 
 int main() {
     stdio_init_all();
+    sleep_ms(1000);
+
+    setup_motor();
+    hall_cen = gpio_get(PIN_HALL_CEN);
+    hall_btw = gpio_get(PIN_HALL_BTW);
+
+    if (clockwise) {
+        if (hall_btw_initial)
+        {
+            state = !hall_cen_initial;            
+            while(true){
+                hall_cen = gpio_get(PIN_HALL_CEN);
+                if(hall_cen && !state)
+                {
+                    state = true;
+                    gpio_put(PIN_CA1, 0);
+                    gpio_put(PIN_CA2, 1);              
+                }
+                else if(!hall_cen && state)
+                {
+                    state = false;
+                    gpio_put(PIN_CA1, 1);
+                    gpio_put(PIN_CA2, 0);
+                }
+
+            }
+        }
+        else
+        {
+            state = hall_cen_initial;
+            while(true){
+                hall_cen = gpio_get(PIN_HALL_CEN);
+                if(hall_cen && state)
+                {
+                    state = false;
+                    gpio_put(PIN_CA1, 0);
+                    gpio_put(PIN_CA2, 1);
+                }
+                else if(!hall_cen && !state)
+                {
+                    state = true;
+                    gpio_put(PIN_CA1, 1);
+                    gpio_put(PIN_CA2, 0);
+                }
+
+            }
+
+        }
+
+    } else {
+        if (hall_btw_initial)
+        {
+            state = !hall_cen_initial;            
+            while(true){
+                hall_cen = gpio_get(PIN_HALL_CEN);
+                if(hall_cen && !state)
+                {
+                    state = true;
+                    gpio_put(PIN_CA1, 1);
+                    gpio_put(PIN_CA2, 0);              
+                }
+                else if(!hall_cen && state)
+                {
+                    state = false;
+                    gpio_put(PIN_CA1, 0);
+                    gpio_put(PIN_CA2, 1);
+                }
+
+            }
+        }
+        else
+        {
+            state = hall_cen_initial;
+            while(true){
+                hall_cen = gpio_get(PIN_HALL_CEN);
+                if(hall_cen && state)
+                {
+                    state = false;
+                    gpio_put(PIN_CA1, 1);
+                    gpio_put(PIN_CA2, 0);
+                }
+                else if(!hall_cen && !state)
+                {
+                    state = true;
+                    gpio_put(PIN_CA1, 0);
+                    gpio_put(PIN_CA2, 1);
+                }
+
+            }
+
+        }
+
+    }
+
+
 
     while (1)
     {
@@ -52,4 +151,24 @@ void pin_blink_program_init(PIO pio, uint sm, uint offset, uint pin) {
    pio_sm_config c = pin_blink_program_get_default_config(offset);
    sm_config_set_set_pins(&c, pin, 1);
    pio_sm_init(pio, sm, offset, &c);
+}
+
+void setup_motor() {
+    gpio_init(PIN_CA1);
+    gpio_set_dir(PIN_CA1, GPIO_OUT);
+    gpio_put(PIN_CA1, 0);
+
+    gpio_init(PIN_CA2);
+    gpio_set_dir(PIN_CA2, GPIO_OUT);
+    gpio_put(PIN_CA2, 0);
+
+    gpio_init(PIN_MTR_ENA);
+    gpio_set_dir(PIN_MTR_ENA, GPIO_OUT);
+    gpio_put(PIN_MTR_ENA, 1);
+
+    gpio_init(PIN_HALL_CEN);
+    gpio_set_dir(PIN_HALL_CEN, GPIO_IN);
+
+    gpio_init(PIN_HALL_BTW);
+    gpio_set_dir(PIN_HALL_BTW, GPIO_IN);
 }
