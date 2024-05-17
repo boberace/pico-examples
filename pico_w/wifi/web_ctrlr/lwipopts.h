d68006ff875873983b6479a9b2fe51a51f4ecca1
boberace <boberace@github.com>
2024-05-17 19:58:03 +0000

    This is a simple web server that runs on the Pico WiFi module. It serves a simple web page that has sliders and buttons that when updated is stored into the pico memory and displayed

---
+++ pico_w/wifi/web_ctrlr/lwipopts.h
@@ -0,0 +1,22 @@
+#ifndef _LWIPOPTS_H
+#define _LWIPOPTS_H
+
+// Generally you would define your own explicit list of lwIP options
+// (see https://www.nongnu.org/lwip/2_1_x/group__lwip__opts.html)
+//
+// This example uses a common include to avoid repetition
+
+#define LWIP_MDNS_RESPONDER 1
+#define LWIP_IGMP 1 //  mdns.c
+#define LWIP_NUM_NETIF_CLIENT_DATA 1 // netif.c
+
+#define MEMP_NUM_SYS_TIMEOUT            (LWIP_NUM_SYS_TIMEOUT_INTERNAL+3) //https://github.com/raspberrypi/pico-sdk/issues/1281
+
+#include "lwipopts_examples_common.h"
+
+// This section enables HTTPD server with SSI, SGI
+// and tells server which converted HTML files to use
+#define LWIP_HTTPD 1
+#define HTTPD_FSDATA_FILE "htmldata.c"
+
+#endif
