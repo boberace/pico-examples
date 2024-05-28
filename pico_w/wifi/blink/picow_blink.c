794c819ddaaa782dc930df94a9147d14fc1445bc
boberace <boberace@github.com>
2024-05-28 21:15:27 +0000

    added uart to picow blink for debug

--- pico_w/wifi/blink/picow_blink.c
+++ pico_w/wifi/blink/picow_blink.c
@@ -7,12 +7,27 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
+#define UART_A_ID uart1
+#define UART_A_BAUD_RATE 115200
+#define UART_A_TX_PIN 0
+#define UART_A_RX_PIN 1
+
+uint setup_uart() {
+    uint uart_ret = uart_init(UART_A_ID, UART_A_BAUD_RATE);
+    gpio_set_function(UART_A_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_A_RX_PIN, GPIO_FUNC_UART);
+    return uart_ret;
+}
+
 int main() {
     stdio_init_all();
     if (cyw43_arch_init()) {
         printf("Wi-Fi init failed");
         return -1;
     }
+    setup_uart();
+    int counter = 0;
+    printf("\r\n\r\n");
     while (true) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
         sleep_ms(100);
@@ -22,5 +37,7 @@ int main() {
         sleep_ms(100);
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
         sleep_ms(700); 
+        printf("\033[A\33[2K\rpicow blink, %i\n", counter);
+        counter++;
     }
 }
